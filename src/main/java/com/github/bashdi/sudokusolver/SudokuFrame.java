9e12a48be177cbd10f4c1458989fe44e924c56dd
Bashdi <sebastianrenzer@gmail.com>
2022-02-07 21:38:51 +0100

    Improved GUI

--- src/main/java/com/github/bashdi/sudokusolver/SudokuFrame.java
+++ src/main/java/com/github/bashdi/sudokusolver/SudokuFrame.java
@@ -11,10 +11,14 @@ public class SudokuFrame extends JFrame {
     final int GRID_SIZE = 9;
     int[][] sudoku = new int[GRID_SIZE][GRID_SIZE];
     HashMap<Point, JTextField> textFieldMap = new HashMap<>();
+    JLabel triesLabel;
 
     public SudokuFrame() {
         setTitle("SudokuSolver");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+
 
         //Layout
         setLayout(new GridBagLayout());
@@ -22,33 +26,82 @@ public class SudokuFrame extends JFrame {
         gbc.insets = new Insets(2, 2,2,2);
         gbc.fill = GridBagConstraints.BOTH;
 
+
+
+        //create sudoku grid with jtextfields
         for (int r = 0; r < GRID_SIZE; r++) {
             for (int c = 0; c < GRID_SIZE; c++) {
                 gbc.gridx = c;
                 gbc.gridy = r;
                 JTextField jTextField = new JTextField();
                 jTextField.setPreferredSize(new Dimension(30, 30));
+                jTextField.setHorizontalAlignment(JTextField.CENTER);
                 add(jTextField, gbc);
                 textFieldMap.put(new Point(c, r), jTextField);
+
+                //color grid
+                if (r < 3 || r >= 6) {
+                    if (c < 3 || c >= 6) {
+                        jTextField.setBackground(Color.orange);
+                    } else {
+                        jTextField.setBackground(Color.yellow);
+                    }
+                } else {
+                    if (c < 3 || c >= 6) {
+                        jTextField.setBackground(Color.yellow);
+                    } else {
+                        jTextField.setBackground(Color.orange);
+                    }
+                }
             }
 
         }
 
+
+
+        //solveButton
         JButton solveButton = new JButton("Solve");
         solveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                solve();
+                solveSodoku();
             }
         });
-        add(solveButton);
+        gbc.gridx = 9;
+        gbc.gridy = 0;
+        add(solveButton, gbc);
+
+
+
+        //clearButton
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearGrid();
+            }
+        });
+        gbc.gridx = 9;
+        gbc.gridy = 1;
+        add(clearButton, gbc);
+
+
+
+        //triesLabel
+        triesLabel = new JLabel();
+        gbc.gridx = 9;
+        gbc.gridy = 2;
+        add(triesLabel, gbc);
+
 
 
         pack();
         setVisible(true);
     }
 
-    public void solve() {
+
+
+    public void solveSodoku() {
         //get sudoku
         for (int r = 0; r < GRID_SIZE; r++) {
             for (int c = 0; c < GRID_SIZE; c++) {
@@ -71,6 +124,18 @@ public class SudokuFrame extends JFrame {
                 textFieldMap.get(new Point(c, r)).setText(String.valueOf(sudoku[r][c]));
             }
         }
+
+        triesLabel.setText("Tries: " + sudokuSolver.getSolveCalls());
+    }
+
+
+    private void clearGrid() {
+        for (int r = 0; r < GRID_SIZE; r++) {
+            for (int c = 0; c < GRID_SIZE; c++) {
+                textFieldMap.get(new Point(c, r)).setText("");
+            }
+        }
+        triesLabel.setText("");
     }
 
 }
